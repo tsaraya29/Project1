a82896a8dfc4b4da789dc7311acf4195dfaa8d2c
T Saraya <saratgrffn@gmail.com>
2019-02-10 21:13:40 -0600

    youtube code

--- js/youtube.js
+++ js/youtube.js
@@ -6,6 +6,7 @@ console.log("Page loaded")
       //prepare the request
       var video = $("#video-input").val().trim();
       var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=viewCount&publishedAfter=2017-01-01T00%3A00%3A00Z&q=" + video + "+recipes&safeSearch=moderate&type=video&videoDefinition=high&key=AIzaSyAxNCs1H9tUxw9laSPZpdc64rwLpMqknMs";
+      var playlist
 
       $.ajax({
           url: queryURL,
@@ -13,6 +14,12 @@ console.log("Page loaded")
       })
        .then(function(response){
            console.log(response);
+           var title = response.items[0].snippet.title;
+           console.log("Title: ", title);
+           var description = response.items[0].snippet.description; 
+           console.log("Description: ",description);
+           var videoId = response.items[0].id.videoId;
+           console.log("This is the video id: ", videoId);
        })
 
       });
