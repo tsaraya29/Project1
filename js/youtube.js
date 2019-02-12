26e9d371f945cca7840ad6d22253a4500f232e1d
tsaraya29 <saratgrffn@gmail.com>
2019-02-12 15:53:22 -0600

    working app

--- js/youtube.js
+++ js/youtube.js
@@ -5,8 +5,7 @@ console.log("Page loaded")
       event.preventDefault();
       //prepare the request
       var video = $("#video-input").val().trim();
-      var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=viewCount&publishedAfter=2017-01-01T00%3A00%3A00Z&q=" + video + "+recipes&safeSearch=moderate&type=video&videoDefinition=high&key=AIzaSyAxNCs1H9tUxw9laSPZpdc64rwLpMqknMs";
-      
+      var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=viewCount&publishedAfter=2017-01-01T00%3A00%3A00Z&q=" + video + "+recipes&safeSearch=moderate&type=video&videoDefinition=high&key=AIzaSyAxNCs1H9tUxw9laSPZpdc64rwLpMqknMs";      
 
       $.ajax({
           url: queryURL,
@@ -20,13 +19,49 @@ console.log("Page loaded")
            console.log("Description: ",description);
            var videoId = response.items[0].id.videoId;
            console.log("This is the video id: ", videoId); //need to append this to https://www.youtube.com/watch?v= WARNING: There may also be a snipped ID preferred
-            function loadVids(){
+           var thumbnail = response.items[0].snippet.thumbnails.default.url;
+           console.log(thumbnail);  
+
+          //run results loop
+           resultsLoop();
+
+
+
+           function mainVid(id) {
+            $('#video').html(`
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            `);
+        }         
+        
+         //loop thru items array and bring back thumbnail, video title and description for each item using jquery .each
+           function resultsLoop(){
+            $("#main").empty();              
+            $.each(response.items, function(i,item) {
+
+                var thumb = item.snippet.thumbnails.default.url;
+                var title = item.snippet.title;
+                var description = item.snippet.description.substring(0,100);
+              
+                $("#main").append(`
+                <article>
+              <img src="${thumb}" alt = "" class="thumb">
+              <div class="details">
+                 <h4>${title}</h4>
+                <p>${description}</p>
+              </div>
+            </article>`
+            );      
+             });
+              };             
+              $('main').on('click', 'article', function () {
+                var id = $(this).attr('data-key');
+                mainVid(videoId);
+            });   
           
-            }
-       })
-       //need to create a function that will display the videos and call that function in the .then
-      })
+            });  //closing .then         
+       }); //closing ajax request      
+      }); // closing main function
 
       
-    });
+
         
